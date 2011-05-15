473be65a19008bb9cb3544380c24979499661015
Michael Frank <msfrank@syntaxjockey.com>
2011-05-15 03:13:11 -0700

    handle file inode or device changes

--- terane/inputs/file.py
+++ terane/inputs/file.py
@@ -35,12 +35,16 @@ class FileInputPlugin(Plugin):
 
 class FileInput(Input):
 
-    def configure(self, section):
+    def __init__(self):
         self._deferred = None
         self._file = None
         self._prevstats = None
         self._position = None
-        self._path = section.getString('file path')
+        self._errno = None
+        Input.__init__(self)
+
+    def configure(self, section):
+        self._path = section.getString('file path', None)
         if self._path == None:
             raise Exception("[input:%s] missing required parameter 'file path'" % self.name)
         logger.debug("[input:%s] path is %s" % (self.name,self._path))
@@ -60,35 +64,66 @@ class FileInput(Input):
         self._deferred.addCallback(self._tail)
         self._deferred.addCallback(self._schedule)
         self._deferred.addErrback(self._error)
-        self._delayed_call = reactor.callLater(self._interval, self._deferred.callback, None)
+        reactor.callLater(self._interval, self._deferred.callback, None)
 
     def _tail(self, unused):
-        # open the file if necessary
-        if self._file == None:
-            self._file = open(self._path, 'r')
-            self._prevstats = os.stat(self._path)
-            self._position = self._prevstats.st_size
-            _currstats = self._prevstats
-        # get the current file stats
-        else:
-            _currstats = os.stat(self._path)
-        # the file inode and/or underlying block device changed, or the file has shrunk
-        if self._prevstats.st_dev != _currstats.st_dev:
-            raise Exception("[input:%s] vfs device changed" % self._path)
-        if self._prevstats.st_ino != _currstats.st_ino:
-            raise Exception("[input:%s] vfs inode changed" % self._path)
+        try:
+            # open the file if necessary
+            if self._file == None:
+                self._file = open(self._path, 'r')
+                self._prevstats = os.stat(self._path)
+                self._position = self._prevstats.st_size
+                _currstats = self._prevstats
+            # get the current file stats
+            else:
+                _currstats = os.stat(self._path)
+        except (IOError,OSError), (errno, errstr):
+            if errno != self._errno:
+                logger.warning("[input:%s] failed to tail file: %s" % (self.name,errstr))
+                self._errno = errno
+            return
+
+        # check if the file inode and/or underlying block device changed
+        f = self._file
+        try:
+            if self._prevstats.st_dev != _currstats.st_dev:
+                raise Exception("[input:%s] vfs device changed" % self._path)
+            if self._prevstats.st_ino != _currstats.st_ino:
+                raise Exception("[input:%s] vfs inode changed" % self._path)
+        except Exception, e:
+            logger.info(str(e))
+            _currstats = os.fstat(self._file.fileno())
+            self._file = None
+            
+        # check if the file has shrunk
         if self._position > _currstats.st_size:
-            raise Exception("[input:%s] file shrank by %i bytes" %
+            logger.info("[input:%s] file shrank by %i bytes" %
                 (self._path, self._position - _currstats.st_size))
+            self._position = _currstats.st_size
+
         # seek to the next byte to read
-        self._file.seek(self._position)
+        f.seek(self._position)
         # loop reading lines until EOF or incomplete line
         while True:
-            self._position = self._file.tell()
-            line = self._file.readline()
-            if line == '' or not line.endswith('\n'):
+            self._position = f.tell()
+            line = f.readline()
+            # if we reached EOF or the line consists entirely of whitespace
+            if line == '' or line.isspace():
                 break
-            self._write(line.rstrip())
+            # if the line is incomplete (not newline-terminated) and
+            # we haven't switched to reading from a new file
+            if not self._file == None and not line.endswith('\n'):
+                break
+            # remove leading and trailing whitespace 
+            line = line.strip()
+            logger.debug("[input:%s] received line: %s" % (self.name,line))
+            # generate default timestamp and hostname, in case later
+            # filters aren't able to add it
+            ts = datetime.datetime.now(tzutc())
+            hostname = socket.getfqdn()
+            fields = {'input':self.name, '_raw':line, 'ts':ts, 'hostname':hostname}
+            self.on_received_event.signal(fields)
+
         # save the old file stats
         self._prevstats = _currstats
 
@@ -96,22 +131,12 @@ class FileInput(Input):
         logger.debug("[input:%s] tail error: %s" % (self.name,str(failure)))
         return failure
 
-    def _write(self, line):
-        # if the line consists entirely of whitespace, then drop it
-        if line == '' or line.isspace():
-            return
-        logger.debug("[input:%s] received line: %s" % (self.name,line))
-        ts = datetime.datetime.now(tzutc())
-        hostname = socket.getfqdn()
-        self.on_received_event.signal({'input':self.name, '_raw':line, 'ts':ts, 'hostname':hostname})
-
     def stopService(self):
         if self._file:
             self._file.close()
         self._file = None
         self._prevstats = None
         self._position = None
-        self._delayed_call = None
         self._deferred = None
         Input.stopService(self)
         logger.debug("[input:%s] stopped input" % self.name)
