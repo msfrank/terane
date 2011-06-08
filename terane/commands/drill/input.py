3445f429e4f7966bd4f38ad3bae28cb7cb777c21
Michael Frank <msfrank@syntaxjockey.com>
2011-06-08 00:55:20 -0700

    moved input modes into Input class

--- terane/commands/drill/input.py
+++ terane/commands/drill/input.py
@@ -4,77 +4,12 @@ from terane.loggers import getLogger
 
 logger = getLogger('terane.commands.drill.input')
 
-class ModeChanged(BaseException):
-    def __init__(self, mode):
-        self.mode = mode
-
-class ViewMode(object):
-    def __init__(self, input):
-        logger.debug("switched to view mode")
-        self._input = input
-        self._input._invalidate()
-
-    def render(self, size, focus):
-        (maxcol,) = size
-        cursor = None
-        if focus:
-            cursor = self.get_cursor_coords(size)
-        return urwid.TextCanvas([' '], maxcol=maxcol, cursor=cursor)
-
-    def keypress(self, size, key):
-        if key == ':':
-            raise ModeChanged(CommandMode(self._input))
-        elif key == 'q':
-            reactor.stop()
-            return None
-        return key
-
-    def get_cursor_coords(self, size):
-        return (0, 0)
-
-class CommandMode(object):
-    def __init__(self, input):
-        logger.debug("switched to command mode")
-        self._input = input
-        self._input._invalidate()
-        self._buffer = []
-        self._offset = 0
-
-    def render(self, size, focus):
-        (maxcol,) = size
-        cursor = None
-        if focus:
-            cursor = self.get_cursor_coords(size)
-        return urwid.TextCanvas([':' + ''.join(self._buffer)], maxcol=maxcol, cursor=cursor)
-
-    def _docommand(self):
-        line = ''.join(self._buffer)
-        line = line.strip()
-        logger.debug("processing command: '%s'" % line)
-        raise ModeChanged(ViewMode(self._input))
-
-    def _backspace(self):
-        if len(self._buffer) > 0:
-            self._buffer.pop()
-
-    def keypress(self, size, key):
-        if key == 'esc':
-            raise ModeChanged(ViewMode(self._input))
-        elif key == 'backspace' or key == 'delete':
-            self._backspace()
-        elif key == 'enter' or key == 'return':
-            self._docommand()
-        elif len(key) == 1:
-            self._buffer.append(key)
-        self._input._invalidate()
-
-    def get_cursor_coords(self, size):
-        (maxcol,) = size
-        return (1 + len(self._buffer), 0)
 
 class Input(urwid.FlowWidget):
     def __init__(self):
-        self._mode = ViewMode(self)
+        # if _buffer is None, we are in view mode
+        self._buffer = None
+        self._offset = 0
 
     def selectable(self):
         return True
@@ -83,17 +18,46 @@ class Input(urwid.FlowWidget):
         return 1
 
     def render(self, size, focus=False):
-        return self._mode.render(size, focus)
+        (maxcol,) = size
+        cursor = None
+        if focus:
+            cursor = self.get_cursor_coords(size)
+        if self._buffer == None:
+            return urwid.TextCanvas([' '], maxcol=maxcol, cursor=cursor)
+        return urwid.TextCanvas([':' + ''.join(self._buffer)], maxcol=maxcol, cursor=cursor)
 
     def keypress(self, size, key):
         # ignore window resize event
         if key == 'window resize':
             return None
-        # forward keypress to mode
-        try:
-            return self._mode.keypress(size, key)
-        except ModeChanged, e:
-            self._mode = e.mode
+        if self._buffer == None:
+            if key == ':':
+                self._buffer = []
+                key = None
+            elif key == 'q':
+                reactor.stop()
+                key = None
+        else:
+            if key == 'esc':
+                self._buffer = None
+                key = None
+            elif key == 'backspace' or key == 'delete':
+                if len(self._buffer) > 0:
+                    self._buffer.pop()
+                key = None
+            elif key == 'enter' or key == 'return':
+                line = ''.join(self._buffer)
+                self._buffer = None
+                line = line.strip()
+                key = "command %s" % line
+            elif len(key) == 1:
+                self._buffer.append(key)
+                key = None
+        self._invalidate()
+        return key
+
 
     def get_cursor_coords(self, size):
-        return self._mode.get_cursor_coords(size)
+        if self._buffer == None:
+            return (0, 0)
+        return (1 + len(self._buffer), 0)
